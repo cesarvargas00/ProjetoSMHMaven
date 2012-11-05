9ea9700896cfc36bc5f04d7ab9d8bde92dc3a5a5
Cesar Vargas <cesarvargas00@gmail.com>
2012-11-05 14:35:11 -0200

    Commit do ControleDeAcesso.java; um filtro de login. Ver comentário dentro da classe

---
+++ src/main/java/br/edu/mackenzie/projetoSMHMaven/filter/ControleDeAcesso.java
@@ -0,0 +1,46 @@
+package br.edu.mackenzie.projetoSMHMaven.filter;
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(servletNames = { "Faces Servlet" })
+public class ControleDeAcesso implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpSession session = req.getSession();
+		if (session.getAttribute("user") != null
+				|| req.getRequestURI().endsWith("login.xhtml")
+				//Adicionar aqui quais URLS não serão filtradas para o login. 
+				//Foi uma maneira que eu achei para fazer isso. Será a melhor pratica?
+				) {
+			chain.doFilter(request, response);
+		} else {
+			HttpServletResponse res = (HttpServletResponse) response;
+			res.sendRedirect("login.xhtml");
+		}
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
