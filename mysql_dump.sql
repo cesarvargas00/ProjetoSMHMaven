567083539ce48d3e901484ebe7de655ee16cecff
Cesar Vargas <cesarvargas00@gmail.com>
2012-11-08 18:17:01 -0200

    Versao Parcial Final

--- mysql_dump.sql
+++ mysql_dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.25a, for osx10.6 (i386)
+-- MySQL dump 10.13  Distrib 5.5.24, for osx10.6 (i386)
 --
--- Host: localhost    Database: bancosmh
+-- Host: localhost    Database: bancoSMH
 -- ------------------------------------------------------
--- Server version	5.5.25a
+-- Server version	5.5.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,9 +31,9 @@ CREATE TABLE `Comentario` (
   PRIMARY KEY (`id`),
   KEY `FK14DFC4012357E1E0` (`post_id`),
   KEY `FK14DFC401EE26A36F` (`owner_id`),
-  CONSTRAINT `FK14DFC401EE26A36F` FOREIGN KEY (`owner_id`) REFERENCES `Usuario` (`id`),
-  CONSTRAINT `FK14DFC4012357E1E0` FOREIGN KEY (`post_id`) REFERENCES `Post` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  CONSTRAINT `FK14DFC4012357E1E0` FOREIGN KEY (`post_id`) REFERENCES `Post` (`id`),
+  CONSTRAINT `FK14DFC401EE26A36F` FOREIGN KEY (`owner_id`) REFERENCES `Usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,6 +42,7 @@ CREATE TABLE `Comentario` (
 
 LOCK TABLES `Comentario` WRITE;
 /*!40000 ALTER TABLE `Comentario` DISABLE KEYS */;
+INSERT INTO `Comentario` VALUES (5,'Mussum ipsum! Que legal!','2012-11-08 18:10:11',1,2),(6,'Bacon Ipsum!!','2012-11-08 18:11:23',1,1);
 /*!40000 ALTER TABLE `Comentario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,11 +57,15 @@ CREATE TABLE `Post` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `content` longtext,
   `dateOfCreation` datetime DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
   `owner_id` bigint(20) DEFAULT NULL,
+  `contentTextile` longtext,
   PRIMARY KEY (`id`),
   KEY `FK260CC07CC97210` (`owner_id`),
-  CONSTRAINT `FK260CC07CC97210` FOREIGN KEY (`owner_id`) REFERENCES `Usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+  KEY `FK260CC0EE26A36F` (`owner_id`),
+  CONSTRAINT `FK260CC07CC97210` FOREIGN KEY (`owner_id`) REFERENCES `Usuario` (`id`),
+  CONSTRAINT `FK260CC0EE26A36F` FOREIGN KEY (`owner_id`) REFERENCES `Usuario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +74,7 @@ CREATE TABLE `Post` (
 
 LOCK TABLES `Post` WRITE;
 /*!40000 ALTER TABLE `Post` DISABLE KEYS */;
-INSERT INTO `Post` VALUES (1,'Teste\r\nteste2','2012-11-07 02:37:32',2),(2,'Testetest','2012-11-07 02:41:49',2),(3,'Testetest','2012-11-07 02:42:11',2),(4,'Testetest\r\nTeste','2012-11-07 02:42:29',2),(5,'Teste','2012-11-07 02:44:03',NULL),(6,'Teste de postagem','2012-11-07 02:45:20',2),(7,'	<p>Teste 22</p>','2012-11-07 02:50:20',2),(8,'	<p>Teste 22</p>','2012-11-07 02:51:43',NULL),(9,'<p>Teste<br />\nteste alisosn <strong>negrito</strong> teste <em>teste</em><br />\nTeste 2</p>','2012-11-07 02:53:04',2),(10,'<p>Teste</p>\n\n	<p>teste alisosn <strong>negrito</strong> teste <em>teste</em></p>\n\n	<p>Teste 2</p>','2012-11-07 02:53:18',2),(11,'<p>Teste</p>\n\n	<p>Teste 2</p>\n\n	<p>Teste 3</p>','2012-11-07 02:57:09',2),(12,'<p>Teste 2</p>\n\n	<p>teste3</p>','2012-11-07 02:58:51',1);
+INSERT INTO `Post` VALUES (1,'<p>Bacon ipsum dolor sit amet swine cow pig venison, pork loin shoulder salami pork belly rump tongue ham hock. T-bone rump tri-tip short loin short ribs doner ham hock, brisket boudin meatloaf cow frankfurter spare ribs capicola bresaola. Strip steak cow jerky meatloaf, pastrami pork loin capicola venison. Capicola bacon ball tip ham hock pig cow. Meatloaf shankle beef turducken. Kielbasa tri-tip doner frankfurter, short loin hamburger jerky boudin short ribs. Fatback andouille rump cow tail jerky.</p>\n\n	<p>Beef ribs pork chop chicken doner, flank boudin spare ribs t-bone meatloaf cow tri-tip. Boudin pancetta ham hock, pig ribeye chuck pastrami turducken fatback pork belly sausage tri-tip tenderloin chicken. Shankle pork t-bone flank, spare ribs tenderloin pancetta biltong. Short ribs ham shankle tri-tip pork chop.</p>\n\n	<p>Tongue kielbasa meatball sirloin, short loin salami shoulder ham hock filet mignon jerky. Ham hock hamburger doner, beef turducken ribeye tenderloin short loin pork belly biltong cow. Sausage ball tip t-bone, cow tail pig swine. Bacon filet mignon hamburger, cow pastrami andouille sirloin jowl chicken boudin venison tail pork belly. Sirloin capicola shank ham hock, shankle frankfurter spare ribs andouille kielbasa tail beef pastrami.</p>','2012-11-08 18:11:10','Testando Post 2',1,'Bacon ipsum dolor sit amet swine cow pig venison, pork loin shoulder salami pork belly rump tongue ham hock. T-bone rump tri-tip short loin short ribs doner ham hock, brisket boudin meatloaf cow frankfurter spare ribs capicola bresaola. Strip steak cow jerky meatloaf, pastrami pork loin capicola venison. Capicola bacon ball tip ham hock pig cow. Meatloaf shankle beef turducken. Kielbasa tri-tip doner frankfurter, short loin hamburger jerky boudin short ribs. Fatback andouille rump cow tail jerky.\r\n\r\nBeef ribs pork chop chicken doner, flank boudin spare ribs t-bone meatloaf cow tri-tip. Boudin pancetta ham hock, pig ribeye chuck pastrami turducken fatback pork belly sausage tri-tip tenderloin chicken. Shankle pork t-bone flank, spare ribs tenderloin pancetta biltong. Short ribs ham shankle tri-tip pork chop.\r\n\r\nTongue kielbasa meatball sirloin, short loin salami shoulder ham hock filet mignon jerky. Ham hock hamburger doner, beef turducken ribeye tenderloin short loin pork belly biltong cow. Sausage ball tip t-bone, cow tail pig swine. Bacon filet mignon hamburger, cow pastrami andouille sirloin jowl chicken boudin venison tail pork belly. Sirloin capicola shank ham hock, shankle frankfurter spare ribs andouille kielbasa tail beef pastrami.'),(2,'<p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>\n\n	<p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>\n\n	<p>Casamentiss faiz malandris se pirulitá, Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer Ispecialista im mé intende tudis nuam golada, vinho, uiski, carirí, rum da jamaikis, só num pode ser mijis. Adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>','2012-11-08 18:09:02','Testando Post 1',1,'Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.\r\n\r\nSuco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.\r\n\r\nCasamentiss faiz malandris se pirulitá, Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Lorem ipsum dolor sit amet, consectetuer Ispecialista im mé intende tudis nuam golada, vinho, uiski, carirí, rum da jamaikis, só num pode ser mijis. Adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.');
 /*!40000 ALTER TABLE `Post` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +95,7 @@ CREATE TABLE `Usuario` (
   `password` varchar(32) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +104,7 @@ CREATE TABLE `Usuario` (
 
 LOCK TABLES `Usuario` WRITE;
 /*!40000 ALTER TABLE `Usuario` DISABLE KEYS */;
-INSERT INTO `Usuario` VALUES ('Usuario',1,NULL,'alissonperez@gmail.com','Alisson','Perez','202cb962ac59075b964b07152d234b70'),('Admin',2,NULL,'alissonperez2@gmail.com','Alisson ADM','Perez','202cb962ac59075b964b07152d234b70');
+INSERT INTO `Usuario` VALUES ('Admin',1,'nenhumPorEnquanto','root','Joaquim','','7b24afc8bc80e548d66c4e7ff72171c5');
 /*!40000 ALTER TABLE `Usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -112,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-11-07  3:07:11
+-- Dump completed on 2012-11-08 18:15:46
