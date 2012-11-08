e61a542c05c2b8e75ec003b9a04a67c31ac87330
Alisson Perez <alissonperez@gmail.com>
2012-11-08 02:43:11 -0200

    Exibindo li's no na barra corretamente

--- src/main/java/br/edu/mackenzie/projetoSMHMaven/beans/UsuarioBean.java
+++ src/main/java/br/edu/mackenzie/projetoSMHMaven/beans/UsuarioBean.java
@@ -4,13 +4,24 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import br.edu.mackenzie.projetoSMHMaven.model.Usuario;
+import br.edu.mackenzie.projetoSMHMaven.util.SessaoUsuario;
 
 @ManagedBean
 @SessionScoped
 public class UsuarioBean {
 	
-	private Usuario user ;
-	
-	private Boolean isLogged ;
+	/**
+	 * @return the user
+	 */
+	public Usuario getUser() {
+		return SessaoUsuario.getInstance().getUser() ;
+	}
+
+	/**
+	 * @return the isLogged
+	 */
+	public Boolean getIsLogged() {
+		return SessaoUsuario.getInstance().isLogged() ;
+	}
 
 }
