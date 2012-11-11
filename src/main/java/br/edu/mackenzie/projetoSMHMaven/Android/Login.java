12dbf1baf1740c13833aa4391d768e4abc8b740c
Alisson Perez <alissonperez@gmail.com>
2012-11-11 19:22:42 -0200

    Servlet para fazer login via android usando JSON

---
+++ src/main/java/br/edu/mackenzie/projetoSMHMaven/Android/Login.java
@@ -0,0 +1,44 @@
+package br.edu.mackenzie.projetoSMHMaven.Android;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter() ;
+		out.print("Fazendo get... Tá errado.." ) ;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String param1 = request.getParameter( "id" ) ;
+		String param2 = request.getParameter( "stringdata" ) ;
+		
+		PrintWriter out = response.getWriter() ;
+		out.print("Retorno: " + param1 + " - " + param2 ) ;
+	}
+
+}
